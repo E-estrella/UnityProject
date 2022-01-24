012430d8d3151aa3d6d9cfc431b33593f33cb90f
E-estrella <5eunbyeol8@gmail.com>
2022-01-24 11:38:17 +0900

    player

--- Assets/Scripts/Change.cs
+++ Assets/Scripts/Change.cs
@@ -7,7 +7,7 @@ public class Change : MonoBehaviour
 {
     public void SceneChange()
     {
-        SceneManager.LoadScene("GamePage");
+        SceneManager.LoadScene("ground");
     }
 
     // Start is called before the first frame update
