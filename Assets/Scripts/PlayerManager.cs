77cff9f49c2ee9598753a5242c917b9314796fab
E-estrella <5eunbyeol8@gmail.com>
2022-01-24 14:30:30 +0900

    Yeji

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
-    //public Text characterName1;
+    //private InputField1 Player1;
+
+    //public Text PlayerName1;
+
     //public Text characterName2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InputField1 InputField1 = GameObject.Find("InputField1").GetComponent<InputField1>();
+        //PlayerName1 = InputField1.Player1;
     }
 
     // Update is called once per frame
