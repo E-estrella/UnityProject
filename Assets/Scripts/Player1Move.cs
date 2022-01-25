c2116ab7df88327363ef7d10faa5bc4147e5f9aa
E-estrella <5eunbyeol8@gmail.com>
2022-01-25 21:05:41 +0900

    Yeji

---
+++ Assets/Scripts/Player1Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Move : MonoBehaviour
+{
+    // 플레이어1 닉네임 변수
+    string P1Name;
+
+    // text 변수
+    public Text text1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerManager PM = GameObject.Find("Player1").GetComponent<PlayerManager>();
+        P1Name = PM.PlayerName1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -19,7 +19,6 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(PlayerName1);
-        Debug.Log(PlayerName2);
+        
     }
 }
