4fc11ba3f8a7070f643638867d876dbf40960771
E-estrella <5eunbyeol8@gmail.com>
2022-01-27 09:42:39 +0900

    씬전환

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,17 +13,17 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         // GameName에 있는 변수 불러오기
-        GameName gamaname  = GameObject.Find("NameManagement").GetComponent<GameName>();
+        GameName gamename  = GameObject.Find("NameManagement").GetComponent<GameName>();
         //PlayerName1 = gamaname.Player1;
         //PlayerName2 = gamaname.Player2;
 
         if (gameObject.name == "Player1")
         {
-            PlayerName.text = gamaname.Player1;
+            PlayerName.text = gamename.Player1;
         }
         else if (gameObject.name == "Player2")
         {
-            PlayerName.text = gamaname.Player2;
+            PlayerName.text = gamename.Player2;
         }
     }
 
