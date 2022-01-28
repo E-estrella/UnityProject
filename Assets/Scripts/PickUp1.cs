1ca1e70730ae33426ca25d8bed90afe91bf4c8bf
rkdmfghd <rkdmfghd@naver.com>
2022-01-28 12:54:08 +0900

    r

--- Assets/Scripts/PickUp1.cs
+++ Assets/Scripts/PickUp1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickUp1 : MonoBehaviour
 {
     public GameObject explosionFactory;
+    public GameObject explosionFactory2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Player"))
@@ -13,13 +14,17 @@ public class PickUp1 : MonoBehaviour
             // 0부터 9(10-1) 까지 값중에 하나를 랜덤으로 가져와서
             int randValue = UnityEngine.Random.Range(0, 10);
             // 만약 3보다 작으면 플레이어방향
-            if (randValue < 6)
+            if (randValue < 5)
             {
                 GameObject explosion = Instantiate(explosionFactory);
                 explosion.transform.position = transform.position;
             }
             // 그렇지 않으면 아래방향으로 정하고 싶다.
-            
+            else
+            {
+                GameObject explosion = Instantiate(explosionFactory2);
+                explosion.transform.position = transform.position;
+            }
 
         }
     }
