8836f08f03ec5b1e13e3c5df1268f2e7f67917e9
rkdmfghd <rkdmfghd@naver.com>
2022-02-01 22:16:26 +0900

    s

---
+++ Assets/Scripts/Inventory1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory1 : MonoBehaviour
+{
+    public List<SlotData1> slots = new List<SlotData1>();
+    private int maxSlot = 11;
+    public GameObject slotPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject slotPanel = GameObject.Find("Panel1");
+        for (int i=0; i < maxSlot; i++)
+        {
+            GameObject go =Instantiate(slotPrefab, slotPanel.transform, false);
+            go.name = "Slot_" + i;
+            SlotData1 slot = new SlotData1();
+            slot.isEmpty = true;
+            slot.slotObj = go;
+            slots.Add(slot);
+        }
+    }
+
+}
--- Assets/Scripts/PickUp1.cs
+++ Assets/Scripts/PickUp1.cs
@@ -6,7 +6,7 @@ public class PickUp1 : MonoBehaviour
 {
     public GameObject explosionFactory;
     public GameObject explosionFactory2;
-    //public GameObject slotItem;
+    public GameObject slotItem;
     //오디오 클립
     public AudioClip Boom;
     public AudioClip Get;
@@ -27,7 +27,7 @@ public class PickUp1 : MonoBehaviour
                 //Destroy(this.gameObject);
                 //오디오 할당(꽝일 때)
                 AudioSource.PlayClipAtPoint(Boom, transform.position);
-
+                Destroy(this.gameObject);
             }
             // 그렇지 않으면
             else
@@ -36,8 +36,23 @@ public class PickUp1 : MonoBehaviour
                 explosion.transform.position = transform.position;
                 AudioSource.PlayClipAtPoint(Get, transform.position);
                 //Destroy(this.gameObject);
+                if (collision.tag.Equals("Player1"))
+                {
+                    Inventory1 inven = collision.GetComponent<Inventory1>();
+                    for (int i = 0; i < inven.slots.Count; i++)
+                    {
+                        if(inven.slots[i].isEmpty)
+                        {
+                            Instantiate(slotItem, inven.slots[i].slotObj.transform, false);
+                            inven.slots[i].isEmpty = false;
+                            Destroy(this.gameObject);
+                            break;
+                        }
+
+                    }
+                }
             }
-            Destroy(this.gameObject);     
+            //Destroy(this.gameObject);     
         }
             
 
