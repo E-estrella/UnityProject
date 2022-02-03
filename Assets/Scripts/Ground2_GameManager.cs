dcc0f33025fbd58416819cec5996c11a4b72e86a
E-estrella <5eunbyeol8@gmail.com>
2022-02-04 08:56:37 +0900

    타이머 수정

--- Assets/Scripts/Ground2_GameManager.cs
+++ Assets/Scripts/Ground2_GameManager.cs
@@ -5,23 +5,23 @@ using UnityEngine.UI;
 
 public class Ground2_GameManager : MonoBehaviour
 {
-    public Text timeText;
-    public float time;
+    //public Text timeText;
+    //public float time;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        time = 180f;
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    time = 180f;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (time > 0)
-        {
-            time -= Time.deltaTime;
-        }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (time > 0)
+    //    {
+    //        time -= Time.deltaTime;
+    //    }
 
-        timeText.text = Mathf.Ceil(time).ToString();
-    }
+    //    timeText.text = Mathf.Ceil(time).ToString();
+    //}
 }
--- Assets/Scripts/g2_time.cs
+++ Assets/Scripts/g2_time.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class g1_time : MonoBehaviour
+public class g2_time : MonoBehaviour
 {
-    [SerializeField] float setTime = 60.0f;
+    [SerializeField] float setTime = 180.0f;
     [SerializeField] Text countdownText;
+
+    [SerializeField]
+    Text timerText;
 
     //GameObject popup = GameObject.Find("Panel");
 
     // Start is called before the first frame update
     void Start()
     {
-        countdownText.text = setTime.ToString();
+        
     }
 
     // Update is called once per frame
@@ -25,12 +28,5 @@ public class g1_time : MonoBehaviour
             Time.timeScale = 0.0f;
 
         countdownText.text = Mathf.Round(setTime).ToString();
-        
-        //// 시간이 소진되면 팝업창
-        //if (gm.time <= 0)
-        //{
-        //    Time.timeScale = 0f;
-        //    popup.SetActive(true);
-        //}
     }
 }
