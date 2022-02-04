2d323c6135ff48886ca309760988ef5c34bb9a70
rkdmfghd <rkdmfghd@naver.com>
2022-02-04 14:42:30 +0900

    포션 플레이어2
    플레이어2의 물약 채워지는 순서 변경

--- Assets/Scripts/PickUp1.cs
+++ Assets/Scripts/PickUp1.cs
@@ -54,7 +54,7 @@ public class PickUp1 : MonoBehaviour
                 else if (collision.tag.Equals("player2"))
                 {
                     Inventory2 inven = collision.GetComponent<Inventory2>();
-                    for (int i = 0; i < inven.slots.Count; i++)
+                    for (int i = inven.slots.Count-1; i >= 0; i--)
                     {
                         if (inven.slots[i].isEmpty)
                         {
