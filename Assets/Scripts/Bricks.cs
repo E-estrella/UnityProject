31f9fcac84bab1899a52df35d03049fcb677523c
iyeni <dldpdms5316@naver.com>
2022-02-06 01:04:21 +0900

    1

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -7,37 +7,37 @@ public class Bricks : MonoBehaviour
 {
     public GameObject go;
     public Tilemap tilemap;
-    GridLayout GridLayout;
+    GridLayout gridLayout;
+    Vector3Int cellPosition;
   
     // Start is called before the first frame update
     void Start()
     {
         tilemap = GetComponent<Tilemap>();
-        //GridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        gridLayout = transform.parent.GetComponentInParent<GridLayout>();
     }
 
 
-   
 
-    public void MakeColor (Vector3 Pos)
+
+    public void MakeColor(Vector3 Pos)
     {
         this.tilemap.RefreshAllTiles();
         Vector3Int cellPosition = tilemap.WorldToCell(Pos);
-        
 
-        //if (Input.GetKey(KeyCode.W | KeyCode.A | KeyCode.S | KeyCode.D))
-        //{
-            tilemap.SetTileFlags(cellPosition, TileFlags.None); //타일 색 바꾸기
-            tilemap.SetColor(cellPosition, (Color.red)); //빨간색
+        if (Input.GetKey(KeyCode.W | KeyCode.A | KeyCode.S | KeyCode.D))
+        {
+            tilemap.SetTileFlags(cellPosition, TileFlags.None); //???? ?? ??????
+            tilemap.SetColor(cellPosition, (Color.red)); //??????
 
-        //}
-            
+        }
+          
             
 
         //if (Input.GetKey(KeyCode.RightArrow | KeyCode.LeftArrow | KeyCode.UpArrow | KeyCode.DownArrow))
         //{
-        //    this.tilemap.SetTileFlags(cellPosition, TileFlags.None); //타일 색 바꾸기
-        //    this.tilemap.SetColor(cellPosition, (Color.blue)); //파란색 색칠
+        //    this.tilemap.SetTileFlags(cellPosition, TileFlags.None); //???? ?? ??????
+        //    this.tilemap.SetColor(cellPosition, (Color.blue)); //?????? ????
         //}
             
 
@@ -47,10 +47,12 @@ public class Bricks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         //if (Input.GetKey("left shift"))
-            //sr.material.color = Color.red; //빨간색
+        //sr.material.color = Color.red; //??????
 
         //if (Input.GetKey("right shift"))
-            //sr.material.color = Color.blue; //파란색 색칠
+        //sr.material.color = Color.blue; //?????? ????
     }
+
 }
