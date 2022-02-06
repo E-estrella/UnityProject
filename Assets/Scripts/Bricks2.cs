f8b37e67d724ec68fd0c8f031b25aa70dd3c8015
Hyeju02 <handaree02@naver.com>
2022-02-07 01:22:37 +0900

    결과창 설정

--- Assets/Scripts/Bricks2.cs
+++ Assets/Scripts/Bricks2.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class Bricks2 : MonoBehaviour
 {
     public GameObject go;
     public Tilemap tilemap;
     GridLayout gridLayout;
+    public Text Player2ScoreUI;
+    public int Player2Score = 0;
+    public Bricks1 Player1score;
+    public g2_time settime;
+    GameObject player2;
 
     // Start is called before the first frame update
     void Start()
     {
         tilemap = GetComponent<Tilemap>();
+        settime = GameObject.Find("Main Camera").GetComponent<g2_time>();
         gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        Player1score = GameObject.Find("Tilemap").GetComponent<Bricks1>();
+
+        player2 = GameObject.Find("Canvas").transform.Find("Panel").transform.Find("winnerP2").gameObject;
+        player2.SetActive(false);
     }
 
     public void MakeColor(Vector3 Pos)
@@ -26,21 +37,50 @@ public class Bricks2 : MonoBehaviour
         int numRed = 0;
         Vector3Int mapPos = new Vector3Int();
 
+        //if (settime.setTime > 0)
+        //{
+
         for (int i = 0; i < tilemap.size.x; i++)
         {
-            mapPos.x = tilemap.origin.x + i;
-
-            for (int j = 0; j < tilemap.size.y; j++)
+            if(settime.setTime > 0)
             {
-                mapPos.y = tilemap.origin.y + j;
+                mapPos.x = tilemap.origin.x + i;
 
-                if (tilemap.GetColor(mapPos) == Color.clear)
+                for (int j = 0; j < tilemap.size.y; j++)
                 {
-                    ++numRed;
+                    mapPos.y = tilemap.origin.y + j;
+
+
+                    if (tilemap.GetColor(mapPos) == Color.clear)
+                    {
+                        ++numRed;
+                        Player2Score = numRed;
+                    }
                 }
             }
-        }
+            if (settime.setTime <= 0)
+            {
+                print("num clear: " + numRed);
+                Player2ScoreUI.text = " X " + Player2Score;
+                
+            }
 
-        print("num clear: " + numRed);
+        }
+        print("num red: " + numRed);
+        Debug.Log(Player2Score);
+        if (Player2Score < Player1score.Player1Score)
+        {
+            player2.SetActive(true);
+        }
+        Player2ScoreUI.text = " X " + Player2Score;
+        
+
     }
 }
+        //}
+        //else if(settime.setTime <= 0)
+        //{
+        //    print("num clear: " + numRed);
+        //    Player2ScoreUI.text = " X " + Player2Score;
+        //}    
+     
