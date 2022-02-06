fd4b082b140c532a1d81519ffbbea7109023a5ca
rkdmfghd <rkdmfghd@naver.com>
2022-02-06 19:36:56 +0900

    ground2 포션나타내기 시도

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject slotItem;
+    public GameObject slotItem2;
+    
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+    GameObject p1Score = GameObject.Find("ScoreManagner");
+    Score p1score = p1Score.GetComponent<Score>();
+
+    GameObject p2Score = GameObject.Find("ScoreManagner");
+    Score p2score = p2Score.GetComponent<Score>();
+
+    Inventory3 inven3 = GetComponent<Inventory3>();
+
+
+
+    int i;
+    for (i = 0; i < p1score; i++)
+    {
+        if (inven3.slots[i].isEmpty)
+        {
+            Instantiate(slotItem, inven3.slots[i].slotObj3.transform, false);
+            inven3.slots[i].isEmpty = false;
+            //Destroy(this.gameObject);
+            break;
+        }
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
