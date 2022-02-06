003c14d2a7c11cef02241a29ec4702a1d90873f8
iyeni <dldpdms5316@naver.com>
2022-02-07 03:40:05 +0900

    Update Stop.cs

--- Assets/Scripts/Stop.cs
+++ Assets/Scripts/Stop.cs
@@ -4,76 +4,44 @@ using UnityEngine;
 
 public class Stop : MonoBehaviour
 {
-   private bool state;
-   bool MonstergameObject = true;
-   public GameObject Monster;
-   public GameObject player1_outter;
-    //float Timer = 0.0f;
+
+    public GameObject Monster;
+    bool collgameObject = true;
+    float Timer = 0.0f;
 
     void Start()
-    {
-        state = true;
-        //if(Player1.gameObject == false)
-        //{
-        //  Timer += Time.deltaTime;
-
-        //if(Timer == 3.0f)
-        // {
-        //  Player1.gameObject.SetActive(true);
-        //   Timer = 0.0f;
-        //  }
-        //}
-
+    { 
 
     }
-    
 
 
-    //destroy
     void OnTriggerEnter2D(Collider2D collision)
     {
-        
-       // if (collision.gameObject.tag.Equals("Monster"))
-       // {
-            //player1_outter.gameObject.SetActive(false);
-           Destroy(collision.gameObject);
 
-       // }
+        if (collision.gameObject.tag.Equals("Monster"))
+        {
+            collision.gameObject.SetActive(false);
+            //Destroy(collision.gameObject);
+
+        }
     }
 
-    
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-
-    //MonstergameObject = false;
-    //Monster.gameObject.SetActive(false);
-    //}
-
 
     void Update()
     {
 
-
-        if(!collgameObject)
+        if (!collgameObject)
         {
-          Timer += Time.deltaTime;
+            Timer += Time.deltaTime;
 
+            if (Timer <= 3.0f)
+            {
+                Monster.gameObject.SetActive(true);
+                collgameObject = true;
 
-          if(Timer <= 3.0f)
-           {
-             Monster.gameObject.SetActive(true);
-             collgameObject = true;
-
-
-             Timer = 0.0f;
-           }
+                Timer = 0.0f;
+            }
         }
 
-
-
-    void Update()
-    {
-         
     }
 }
