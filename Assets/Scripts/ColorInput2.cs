b6838fc296d9f90018a3ff2ca8cb81884c08aa6c
E-estrella <5eunbyeol8@gmail.com>
2022-02-06 15:45:47 +0900

    ground2 수정

--- Assets/Scripts/ColorInput2.cs
+++ Assets/Scripts/ColorInput2.cs
@@ -13,13 +13,6 @@ public class ColorInput2 : MonoBehaviour
         Gizmos.DrawSphere(PlayerPosition, 0.2f);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
